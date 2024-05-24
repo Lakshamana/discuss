785a7eeab63358f9c527959e7b2197f2fc8414d9
Lakhshamana <lakshamana@protonmail.com>
2024-05-24 11:36:55 -0300

    feat: create user login
    thanks mix phx.gen.auth!

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -1,10 +1,13 @@
 defmodule DiscussWeb.Router do
   use DiscussWeb, :router
 
+  import DiscussWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
+    plug :fetch_current_user
     plug :put_root_layout, html: {DiscussWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
@@ -12,18 +15,6 @@ defmodule DiscussWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", DiscussWeb do
-    pipe_through :browser
-
-    get "/", Redirect, to: "/posts"
-    live "/posts", PostLive.Index, :index
-    live "/posts/new", PostLive.Index, :new
-    live "/posts/:slug/edit", PostLive.Index, :edit
-
-    live "/posts/:slug", PostLive.Show, :show
-    live "/posts/:slug/show/edit", PostLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
@@ -45,6 +36,52 @@ defmodule DiscussWeb.Router do
 
       live_dashboard "/dashboard", metrics: DiscussWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", DiscussWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{DiscussWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", DiscussWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{DiscussWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", DiscussWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{DiscussWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+
+      get "/", Redirect, to: "/posts"
+      live "/posts", PostLive.Index, :index
+      live "/posts/new", PostLive.Index, :new
+      live "/posts/:slug/edit", PostLive.Index, :edit
+
+      live "/posts/:slug", PostLive.Show, :show
+      live "/posts/:slug/show/edit", PostLive.Show, :edit
     end
   end
 end
